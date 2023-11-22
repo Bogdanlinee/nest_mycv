ef053ab8a4ae20b031307d13ce63c4cc463c835b
Dan <bogdanlinee@gmail.com>
2023-11-22 20:35:19 +0200

    Finished setting up Users Service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import {Repository} from 'typeorm';
 import {InjectRepository} from '@nestjs/typeorm';
 import {User} from './user.entity';
@@ -13,8 +13,12 @@ export class UsersService {
         return this.repo.save(user);
     }
 
-    findOne(id: number) {
-        return this.repo.findOneBy({id});
+    async findOne(id: number) {
+        const user = await this.repo.findOneBy({id});
+        if (!user) {
+            throw new NotFoundException('user not found');
+        }
+        return user;
     }
 
     find(email: string) {
@@ -25,14 +29,20 @@ export class UsersService {
         const user = await this.findOne(id);
 
         if (!user) {
-            throw new Error('user not found');
+            throw new NotFoundException('user not found');
         }
 
         Object.assign(user, attrs);
         return this.repo.save(user);
     }
 
-    remove() {
+    async remove(id: number) {
+        const user = await this.findOne(id);
 
+        if (!user) {
+            throw new NotFoundException('user not found');
+        }
+
+        return this.repo.remove(user);
     }
 }
