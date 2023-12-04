36ef90dfb0faf392e050483ed4ee79364388000d
Dan <bogdanlinee@gmail.com>
2023-12-04 12:04:28 +0200

    Finished report service and controller

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -1,27 +1,28 @@
 import {IsLatitude, IsLongitude, IsNumber, IsString, Max, Min} from 'class-validator';
+import {Transform} from 'class-transformer';
 
-export class CreateReportDto {
-    @IsNumber()
-    @Max(1000000)
-    price: number;
-
+export class GetEstimateDto {
     @IsString()
     make: string;
 
     @IsString()
     model: string;
 
+    @Transform(({value}) => parseInt(value))
     @IsNumber()
     @Min(1920)
     @Max(2050)
     year: number
 
+    @Transform(({value}) => parseFloat(value))
     @IsLongitude()
     lng: number
 
+    @Transform(({value}) => parseFloat(value))
     @IsLatitude()
     lat: number
 
+    @Transform(({value}) => parseInt(value))
     @IsNumber()
     @Min(0)
     @Max(1000000)
